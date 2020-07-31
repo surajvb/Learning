3ccaa4a4a6ef972c4f53be0cb1565decbe6b2b5d
Suraj Bolanavar <svbsuraj@gmail.com>
2020-07-31 21:39:50 +0530

    Basic route Implementation

--- React-Concepts/react-router/src/App.js
+++ React-Concepts/react-router/src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <h1> React router demonstration</h1>
+        <Route path="/" exact render={ () => {
+          return <h1> Home </h1>
+        }} />
+        <Route path="/about" exact render={ () => {
+          return <h1> About </h1>
+        }} />
+      </div>
+    </Router>
   );
 }
 
