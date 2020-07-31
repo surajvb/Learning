9cebc1154b458f49c86740c660a3a76e4d14690a
Suraj Bolanavar <svbsuraj@gmail.com>
2020-07-31 21:48:39 +0530

    adding strict match and also rendering a component via route and reading values from the params

--- React-Concepts/react-router/src/App.js
+++ React-Concepts/react-router/src/App.js
@@ -3,17 +3,22 @@ import './App.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
+const User = ({match}) => {
+return <h1> Hello welcome { match.params.username}</h1>
+}
+
 function App() {
   return (
     <Router>
       <div className='App'>
         <h1> React router demonstration</h1>
-        <Route path="/" exact render={ () => {
+        <Route path="/" exact strict render={ () => {
           return <h1> Home </h1>
         }} />
-        <Route path="/about" exact render={ () => {
+        <Route path="/about/" exact strict render={ () => {
           return <h1> About </h1>
         }} />
+        <Route path="/user/:username" exact strict component={User} />
       </div>
     </Router>
   );
