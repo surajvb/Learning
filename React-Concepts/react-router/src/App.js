433a61a6d0015f4e1cdd894a42a11a4f250d0140
Suraj Bolanavar <svbsuraj@gmail.com>
2020-07-31 22:00:55 +0530

    Changing routes based on clicking on links

--- React-Concepts/react-router/src/App.js
+++ React-Concepts/react-router/src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
 import Route from 'react-router-dom/Route';
 
-const User = ({match}) => {
-return <h1> Hello welcome { match.params.username}</h1>
-}
+const User = ({ match }) => {
+  return <h1> Hello welcome {match.params.username}</h1>;
+};
 
 function App() {
   return (
     <Router>
       <div className='App'>
         <h1> React router demonstration</h1>
-        <Route path="/" exact strict render={ () => {
-          return <h1> Home </h1>
-        }} />
-        <Route path="/about/" exact strict render={ () => {
-          return <h1> About </h1>
-        }} />
-        <Route path="/user/:username" exact strict component={User} />
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/about/'>About</Link>
+          </li>
+          <li>
+            <Link to='/user/suraj'>Suraj Logged In</Link>
+          </li>
+        </ul>
+
+        <Route
+          path='/'
+          exact
+          strict
+          render={() => {
+            return <h1> Home </h1>;
+          }}
+        />
+        <Route
+          path='/about/'
+          exact
+          strict
+          render={() => {
+            return <h1> About </h1>;
+          }}
+        />
+        <Route path='/user/:username' exact strict component={User} />
       </div>
     </Router>
   );
